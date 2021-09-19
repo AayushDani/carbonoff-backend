b63ce4455af28b82c93cb7137afc1904aa7069ea
AayushDani <aayushdani1@gmail.com>
2021-09-18 19:34:59 -0500

    url BUG FIX FFS

--- backend/carbon_footprint/urls.py
+++ backend/carbon_footprint/urls.py
@@ -4,7 +4,7 @@ from .views import *
 
 
 urlpatterns = [
-    path('create-footprint', create_footprint, name="create-footprint"),
+    path('create-footprint/', create_footprint, name="create-footprint"),
     path('get-footprints', get_footprints, name="get-footprints"),
     path('get-footprint/<int:footprint_id>',
          get_footprint, name="get-footprint"),
